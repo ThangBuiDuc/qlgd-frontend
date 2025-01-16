45d300a2c9dfecd2a86c503d26c6550202b62db4
ThangBuiDuc <thangbd@hpu.edu.vn>
2025-01-16 22:19:14 +0700

    ready for production

--- app/(app)/layout.jsx
+++ app/(app)/layout.jsx
@@ -6,23 +6,6 @@ import { currentUser } from "@clerk/nextjs/server";
 const Layout = async ({ children }) => {
   const user = await currentUser();
 
-  // console.log(user.emailAddresses[0].emailAddress);
-
-  // if (user)
-  //   return (
-  //     <div className="flex flex-col gap-4">
-  //       <Header />
-  //       <main className="pr-[10vw] pl-[10vw] min-h-[82vh]">
-  //         <div className="flex flex-col gap-3">
-  //           <Search />
-  //           <p>Chào bạn {user.primaryEmailAddress}</p>
-  //           {children}
-  //         </div>
-  //       </main>
-  //       <Footer />
-  //     </div>
-  //   );
-
   return (
     <div className="flex flex-col gap-4">
       <Header />
